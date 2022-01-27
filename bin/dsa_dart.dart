d56f5802cd6817760da97be0dee5191a21912d51
hendrick <sebastinesoacatp@gmail.com>
2022-01-27 17:35:26 +0100

    dsa: stacks

--- bin/dsa_dart.dart
+++ bin/dsa_dart.dart
@@ -1,18 +1,25 @@
+import 'dart:collection';
 import 'package:dsa_dart/dsa_dart.dart' as dsa_dart;
+import 'package:dsa_dart/stack.dart';
 
 void main(List<String> arguments) {
-  final start = DateTime.now();
-  final sum = sumFromOneTo(10000);
-  final end = DateTime.now();
-  final time = end.difference(start);
-  print(sum);
-  print(time);
+  const list = ['S', 'M', 'O', 'K', 'E'];
+  reverseList(list);
+  // final stack = Stack<int>();
+  // stack.push(1);
+  // stack.push(2);
+  // stack.push(3);
+  // stack.push(4);
+
 }
 
-int sumFromOneTo(int n) {
-  var sum = 0;
-  for(var i =1; i<= n; i++) {
-    sum += i;
+void reverseList(List list){
+  final start = DateTime.now();
+  final stack = Stack.of(list);
+  for(final item in list ){
+    print(stack.pop());
   }
-  return sum;
+  final end = DateTime.now();
+  final change = end.difference(start);
+  print('Time taken: ${change.inMilliseconds} millisecs');
 }
