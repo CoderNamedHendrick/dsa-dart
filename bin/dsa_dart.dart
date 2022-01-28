1608709e2c2e00c373ede7a1dd78e6234f0db6b2
hendrick <sebastinesoacatp@gmail.com>
2022-01-28 03:34:05 +0100

    ds: linked lists

--- bin/dsa_dart.dart
+++ bin/dsa_dart.dart
@@ -1,25 +1,60 @@
-import 'dart:collection';
-import 'package:dsa_dart/dsa_dart.dart' as dsa_dart;
+import 'package:dsa_dart/linked_list.dart';
 import 'package:dsa_dart/stack.dart';
 
 void main(List<String> arguments) {
-  const list = ['S', 'M', 'O', 'K', 'E'];
-  reverseList(list);
-  // final stack = Stack<int>();
-  // stack.push(1);
-  // stack.push(2);
-  // stack.push(3);
-  // stack.push(4);
+  final list = LinkedList<int>();
+  list.append(1);
+  list.append(2);
+  list.append(3);
 
+  print('Before: $list');
+
+  var middleNode = list.nodeAt(1)!;
+
+  list.insertAfter(middleNode, 42);
+
+  print('After: $list');
 }
 
-void reverseList(List list){
-  final start = DateTime.now();
-  final stack = Stack.of(list);
-  for(final item in list ){
-    print(stack.pop());
-  }
-  final end = DateTime.now();
-  final change = end.difference(start);
-  print('Time taken: ${change.inMilliseconds} millisecs');
-}
+// void main(List<String> arguments) {
+//   const list = ['S', 'M', 'O', 'K', 'E'];
+//   final string = 'h((e))llo(world)()';
+//   print(balanceTheParentheses(string));
+//   // final stack = Stack<int>();
+//   // stack.push(1);
+//   // stack.push(2);
+//   // stack.push(3);
+//   // stack.push(4);
+//
+// }
+//
+// bool balanceTheParentheses(String string) {
+//   final parenthesesStack = Stack();
+//   final open = '('.codeUnitAt(0);
+//   final close = ')'.codeUnitAt(0);
+//
+//   for(var codeUnit in string.codeUnits) {
+//
+//     if (codeUnit == open) {
+//       parenthesesStack.push(codeUnit);
+//     } else if (codeUnit == close) {
+//       if (parenthesesStack.isEmpty){
+//         return false;
+//       } else {
+//         parenthesesStack.pop();
+//       }
+//     }
+//   }
+//   return parenthesesStack.isEmpty;
+// }
+//
+// void reverseList(List list){
+//   final start = DateTime.now();
+//   final stack = Stack.of(list);
+//   for(final item in list ){
+//     print(stack.pop());
+//   }
+//   final end = DateTime.now();
+//   final change = end.difference(start);
+//   print('Time taken: ${change.inMilliseconds} millisecs');
+// }
